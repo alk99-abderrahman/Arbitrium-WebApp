032c361ea8cf31b921c4bd2ca8efa1d1db19c48d
root <root@kali.demohost>
2021-01-09 17:28:59 -0500

    set API_FQDN

--- setAPI_FQDN.sh
+++ setAPI_FQDN.sh
@@ -6,6 +6,7 @@ cd $SCRIPTPATH
 
 
 value="{API_FQDN}"
+value_IP="{API_FQDN_IP}"
 
 
 for arg in "$@"
@@ -18,19 +19,23 @@ do
 done
 
 
-
 if [ -e .API_FQDN.conf ]
 then
-    value=`cat .API_FQDN.conf` 
+    value=`cat .API_FQDN.conf`
+    value_IP=$(getent hosts $value | awk '{gsub(/\n/,"",$1); print $1; exit}')
 fi
 
 
 
 if [ "$1" == "" ]
 then
-	echo "No domain was provided. Usage: ./setAPI_FQDN.sh DOMAIN_OF_API_HOST_OR_IP"
+    echo "No domain was provided. Usage: ./setAPI_FQDN.sh DOMAIN_OF_API_HOST_OR_IP"
 else
-	echo "$value" > .API_FQDN.conf
-    grep -rl "$value" . --exclude-dir=node_modules --exclude=*setAPI_FQDN.sh | xargs sed -i "s/$value/$1/g"
+    echo "$value" > .API_FQDN.conf
+    resolvedDNS=$(getent hosts $1 | awk '{gsub(/\n/,"",$1); print $1; exit}')
+    grep -rl "$value" . --exclude-dir=WebApp --exclude=*setAPI_FQDN.sh | xargs sed -i "s/$value/$1/g"
+    grep -rl "$value_IP" . --exclude-dir=WebApp --exclude=*setAPI_FQDN.sh | xargs sed -i "s/$value_IP/$resolvedDNS/g"
     echo "[!] Done"
 fi
+
+
